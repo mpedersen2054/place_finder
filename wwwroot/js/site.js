1132512010feb4664161e96bd721b7a1d3c623ab
Matt Pedersen <mpedersen2054@gmail.com>
2017-12-04 23:53:19 -0500

    add handle for submitting review

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,7 @@
 
 $(function() {
     $('#lookup-form').on('submit', lookupFormSubmit)
+    $('.add-review-form').on('submit', addReviewSubmit)
 })
 
 function lookupFormSubmit(e) {
@@ -15,12 +16,29 @@ function lookupFormSubmit(e) {
     $.post(
         '/map/lookup',
         lookupObj
-    ).then((data) => {
+    ).then(data => {
         // console.log('data from /map/lookup!!', data)
         console.log(data.results)
         var map = new Map(data.Coords, lookupObj, data.results)
         map.init()
     }).fail((xhr, status, message) => {
-        console.log('there was an error!', status, message)
+        console.log('there was an error!', xhr, status, message)
+    })
+}
+
+function addReviewSubmit(e) {
+    e.preventDefault()
+    let $this = $(this),
+        placeId = $this.data('placeid'),
+        review = $this.find('.review-inp').val().trim()
+
+    console.log('submitting review', placeId, review)
+    $.post(
+        '/users/add_review',
+        { PlaceId: placeId, Review: review }
+    ).then(data => {
+        console.log('submitted!', data)
+    }).fail((xhr, status, message) => {
+        console.log('there was an error!', xhr, status, message)
     })
 }
